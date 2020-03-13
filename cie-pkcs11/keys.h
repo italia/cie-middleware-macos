6fcdc3e587ea8a145e29e8145832d82d88bff3ee
ugo <me@ugochirico.com>
2020-03-13 17:53:07 +0100

    Completata fix issue #23

--- cie-pkcs11/keys.h
+++ cie-pkcs11/keys.h
@@ -1,6 +1,6 @@
 //
 //  keys.h
-//  cie-pkcs11
+//  cie-pkcs11/Users/ugos/Documents/Projects/cie-middleware-macos/Installer/build/CIE-Middleware.pkg
 //
 //  Created by ugo chirico on 07/01/2019.
 //  Copyright © 2019 IPZS. All rights reserved.
