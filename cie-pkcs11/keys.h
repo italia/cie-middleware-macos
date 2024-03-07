381c0757c5d76fd2652ae110839d392a0ce66458
Antonio Ciccarelli <aci@bit4id.com>
2024-03-07 13:15:09 +0100

    Added support for signing without pairing CIE beforehand. Refactored and prepared the whole environment for App Store release.

--- cie-pkcs11/keys.h
+++ cie-pkcs11/keys.h
@@ -1,15 +1,12 @@
 //
 //  keys.h
-//  cie-pkcs11/Users/ugos/Documents/Projects/cie-middleware-macos/Installer/build/CIE-Middleware.pkg
-//
-//  Created by ugo chirico on 07/01/2019.
 //  Copyright © 2019 IPZS. All rights reserved.
 //
 
 #ifndef keys_h
 #define keys_h
 
-#define ENCRYPTION_KEY "key.cie.middlewre.macos.2019"
+#define ENCRYPTION_KEY "this is a fake key"
 
 
 #endif /* keys_h */
